23067fa1617d06bfb3e2dca26aa4df023a1e546a
brockcarpani <31215823+brockcarpani@users.noreply.github.com>
2018-12-18 16:21:31 -0600

    change testtext to t/f

--- bot.js
+++ bot.js
@@ -8,15 +8,15 @@ var testText;
 function respond() {
   var request = JSON.parse(this.req.chunks[0]),
       botRegex = /^\!dd$/;
-  
-  testText = request.text;
 
   if(request.text && botRegex.test(request.text)) {
     this.res.writeHead(200);
+    testText = "true";
     postMessage();
     this.res.end();
   } else {
     console.log("don't care");
+    testText = "false";
     postMessage();
     this.res.writeHead(200);
     this.res.end();
