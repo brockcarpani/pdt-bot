cc8ddacca782d111c6d8d336a3e92ae9ce921232
brockcarpani <31215823+brockcarpani@users.noreply.github.com>
2018-12-18 16:28:56 -0600

    removed semicolon after hello world

--- bot.js
+++ bot.js
@@ -29,7 +29,7 @@ function postMessage() {
 
   body = {
     "bot_id" : botID,
-    "text" : "Hello world";
+    "text" : "Hello world"
   };
 
   console.log('sending ' + botResponse + ' to ' + botID);
