fb9fa05ec7d127f4423fe562d6c0f49f7cf1cb7f
brockcarpani <31215823+brockcarpani@users.noreply.github.com>
2018-12-18 15:44:42 -0600

    change test back to /cool guy

--- bot.js
+++ bot.js
@@ -5,7 +5,7 @@ var botID = process.env.BOT_ID;
 
 function respond() {
   var request = JSON.parse(this.req.chunks[0]),
-      botRegex = /^\test$/;
+      botRegex = /^\/cool guy$/;
   
   var testChunks = JSON.parse(this.req.chunks[0]);
   var testText = request.text;
